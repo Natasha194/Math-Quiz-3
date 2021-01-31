af63af6abf4011cfac6fba47681ccdcd0aa13efc
Natasha194 <77156649+Natasha194@users.noreply.github.com>
2021-01-31 09:22:34 +0530

    Add files via upload

---
+++ mathQuiz.js
@@ -0,0 +1,9 @@
+function addUser() {
+    var player1name = document.getElementById("Player1name").value;
+    var player2name = document.getElementById("Player2name").value;
+
+    localStorage.setItem("player 1 name", player1name);
+    localStorage.setItem("player 2 name", player2name);
+
+    window.location = "gamePage.html";
+}
